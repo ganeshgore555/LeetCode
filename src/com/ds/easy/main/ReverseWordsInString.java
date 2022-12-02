a7e821b2306d0e082cd2cdcc88cb12d91507e53e
Ganensh Gore <ganeshgore555@gmail.com>
2022-12-02 10:08:27 +0530

    ReverseString

---
+++ src/com/ds/easy/main/ReverseWordsInString.java
@@ -0,0 +1,13 @@
+package com.ds.easy.main;
+
+public class ReverseWordsInString {
+
+	public static void main(String[] args) {
+		String s = "Let's take LeetCode contest";
+		System.out.println(reverseWords(s));
+	}
+
+    public static String reverseWords(String s) {
+		return s;
+    }
+}
